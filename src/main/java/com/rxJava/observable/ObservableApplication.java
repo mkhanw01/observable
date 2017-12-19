1185a808f15d49f320ce1997d15bf39f5c8dec5b
waseem.khan <mohammad.waseem@coviam.com>
2017-12-19 10:48:08 +0530

    firsrt commit

---
+++ src/main/java/com/rxJava/observable/ObservableApplication.java
@@ -0,0 +1,12 @@
+package com.rxJava.observable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObservableApplication.class, args);
+	}
+}
